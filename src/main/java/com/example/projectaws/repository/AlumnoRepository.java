2fca7cde672b6c0863504c7505028197f4411bcc
Mariana González <a19201105@alumnos.uady.mx>
2025-11-28 15:44:49 -0600

    Revert "version 2.0"
    This reverts commit 7f2d8aa68ea5e9f4f88edb6d22bc5ac3b1bf1312.

--- src/main/java/com/example/projectaws/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectaws.repository;
-
-import com.example.projectaws.model.Alumno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
-}
